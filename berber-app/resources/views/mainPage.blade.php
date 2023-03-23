2696e67d606b8476b5f54255f05bfd306571efd8
Mustafacoban <mustafa.coban@std.hku.edu.tr>
2023-03-23 09:14:05 +0300

    mainPage navbar was done

--- berber-app/resources/views/mainPage.blade.php
+++ berber-app/resources/views/mainPage.blade.php
@@ -2,7 +2,29 @@
 
 
 @section('content')
-   <div class=navbar>
-    
+   <div class="navbar-outer">
+      <div class="navbar-inner">
+            <div class="logo">
+               <a href="#default" class="logo-link">
+                  <img class="logo-img" src="/image/razor-icon.png">
+               </a>
+               <p class="logo-title">BERBERİM</p>
+            </div>
+            <div class="reservation">
+               <form class="reservation-form" action="">
+                  <button type="submit" class="btn-reserve">Randevu Al</button>
+               </form>
+            </div>
+            <div class="other-buttons">
+               <p class="user-name">Mustafa</p>
+               <form  class=other-button-form  action="">
+                  <button class="btn">Register</button>
+               </form>
+               <form  class=other-button-form  action="">
+                  <button class="btn">Log out</button>
+               </form>
+               
+            </div>
+      </div>
    </div>
 @endsection
