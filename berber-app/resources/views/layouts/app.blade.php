8f98666eeec1a08440ea26c008d08c364b1d6cbc
Mustafacoban <mustafa.coban@std.hku.edu.tr>
2023-03-24 15:37:55 +0300

    reservation-system will be added

--- berber-app/resources/views/layouts/app.blade.php
+++ berber-app/resources/views/layouts/app.blade.php
@@ -4,8 +4,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    @vite(['resources/css/register.css', 'resources/js/app.js'])
+    @vite(['resources/css/register.css', 'resources/js/app.js','resources/css/app.css'])
     @vite(['resources/css/login.css', 'resources/css/mainpage.css'])
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 </head>
 <body>
--- berber-app/resources/views/mainPage.blade.php
+++ berber-app/resources/views/mainPage.blade.php
@@ -6,7 +6,7 @@
       <div class="navbar-inner">
             <div class="logo">
                <a href="#default" class="logo-link">
-                  <img class="logo-img" src="/image/razor-icon.png">
+                  <img class="logo-img" src="/image/razor-65.png">
                </a>
                <p class="logo-title">BERBERİM</p>
             </div>
@@ -22,6 +22,35 @@
                   <a class="other-button-link" href="{{route('logout')}}"><button class="btn">Log out</button></a>
                @endif 
             </div>
+      </div>
+   </div>
+   <div class="main-content">
+      <img src="/image/barber.png" alt="">
+   </div>
+   <div class="footer">
+      <div class="iletişim">
+         <h3>İLETİŞİM</h3>
+         <P>Phone: 123-456-7890</P>
+         <p>Email:  info@mysite.com</p>
+         <div class="social-meida">
+            <a href="#" class="fa fa-facebook"></a>
+            <a href="#" class="fa fa-twitter"></a>
+            <a href="#" class="fa fa-instagram"></a>
+            <a href="#" class="fa fa-snapchat-ghost"></a>
+         </div>
+      </div>
+      <div class="adres">
+         <h3>OUR ADDRESS</h3>
+         <P>500 Terry Francine Street</P>
+         <p>San Francisco, CA 94158</p>
+
+      </div>
+      <div class="calisma-saatleri">
+         <h3>OPENING HOURS</h3>
+         <P>Mon - Fri: 7am - 10pm</P>
+         <p>Saturday: 8am - 10pm</p>
+         <p>​Sunday: 8am - 11pm</p>
+
       </div>
    </div>
 @endsection
