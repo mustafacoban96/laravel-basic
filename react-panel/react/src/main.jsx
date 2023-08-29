2d7d3b7759777f92252ac167870c2f062a0a43d0
mustafacoban96 <mustafa.coban@std.hku.edu.tr>
2023-08-29 20:58:10 +0300

    router and routes are defined

--- react-panel/react/src/main.jsx
+++ react-panel/react/src/main.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+//import App from './App.jsx'
 import './index.css'
+import { RouterProvider } from 'react-router-dom'
+import router from './router.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
+  // the lines that is above were deleted on the project
+
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router}/>
+  </React.StrictMode>
 )
