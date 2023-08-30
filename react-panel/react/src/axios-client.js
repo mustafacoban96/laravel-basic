5264e086fd938a9ae650cff58b5219d6ab4c7b64
mustafacoban96 <mustafa.coban@std.hku.edu.tr>
2023-08-30 23:04:57 +0300

    AxiosClient-part and authorization

---
+++ react-panel/react/src/axios-client.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+
+const axiosClient = axios.create({
+    baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`
+});
+
+//interceptors
+
+axiosClient.interceptors.request.use((config) =>{
+
+    const token = localStorage.get('ACCESS_TOKEN');
+    config.headers.Authorization = `Bearer ${token}`;
+
+
+    return config;
+});
+
+// include two functions as paramter for fulfilled and rejected
+axiosClient.interceptors.response.use((response) =>{
+    return response
+}, (error) =>{
+    const {response} = error;
+    if(response.status == 401){
+        localStorage.removeItem('ACCESS_TOKEN');
+    }
+
+    throw error;
+})
+
+export default axiosClient;
